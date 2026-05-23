cb6db3220e72d44340a2f374532f580e3cd0be96
anhtuan <hoanganhtuan512007@gmail.com>
2026-05-23 15:53:55 +0700

    LMS-1 fix update exam submission

--- features/course/exam/viewsets/exam_submission_viewset.py
+++ features/course/exam/viewsets/exam_submission_viewset.py
@@ -10,6 +10,21 @@ from features.course.exam.serializers import (
 from features.course.exam.services import ExamSubmissionService
 
 
+def exam_submission_error_response(exc):
+    message = str(exc)
+    status_code = status.HTTP_400_BAD_REQUEST
+
+    if message in {"Exam not found", "Resource not found"}:
+        status_code = status.HTTP_404_NOT_FOUND
+    elif message in {
+        "Student is not a member of this classroom",
+        "Resource does not belong to this student",
+    }:
+        status_code = status.HTTP_403_FORBIDDEN
+
+    return Response({"error": message}, status=status_code)
+
+
 class ConsumerExamSubmissionViewSet(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -19,11 +34,15 @@ class ConsumerExamSubmissionViewSet(APIView):
         serializer = ExamSubmissionRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        submission, created = self.submission_service.submit_exam(
-            exam_id=exam_uid,
-            student_id=request.user.uid,
-            data=serializer.validated_data,
-        )
+        try:
+            submission, created = self.submission_service.submit_exam(
+                exam_id=exam_uid,
+                student_id=request.user.uid,
+                data=serializer.validated_data,
+            )
+        except ValueError as exc:
+            return exam_submission_error_response(exc)
+
         response_status = status.HTTP_201_CREATED if created else status.HTTP_200_OK
         return Response(serialize_exam_submission(submission), status=response_status)
 
@@ -41,8 +60,8 @@ class ConsumerMyExamSubmissionViewSet(APIView):
             )
         except ValueError as exc:
             if str(exc) == "Submission not found":
-                return Response({"error": str(exc)}, status=status.HTTP_404_NOT_FOUND)
-            raise
+                return Response(None, status=status.HTTP_200_OK)
+            return exam_submission_error_response(exc)
 
         return Response(serialize_exam_submission(submission))
 
